a8deeeea823063322f0f39e7e2db8275da19c889
MapasLPRM <projetoufeslprm@gmail.com>
2016-10-26 11:05:01 -0200

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -5,18 +5,22 @@
 		'message'=>'Obrigado por nos contatar. Entraremos em contato em breve!'
 	);
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+    if(isset($_POST["submit"])){
 
-    $email_from = $email;
-    $email_to = 'lukasfabioteste@gmail.com';
+        $name = @trim(stripslashes($_POST['name'])); 
+        $email = @trim(stripslashes($_POST['email'])); 
+        $subject = @trim(stripslashes($_POST['subject'])); 
+        $message = @trim(stripslashes($_POST['message'])); 
 
-    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+        $email_from = $email;
+        $email_to = 'lukasfabio@hotmail.com';
 
-    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+        $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
-    echo json_encode($status);
+        $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+
+        echo json_encode($status);
+
+    }
     die;
 ?>
